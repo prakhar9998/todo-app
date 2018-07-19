636c2131decfdad0dbd90d62b31cd89198a8e686
Prakhar Varshney <prakhar.varshney33@gmail.com>
2018-07-19 14:45:00 +0530

    list display functionality

--- src/App.js
+++ src/App.js
@@ -1,21 +1,71 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
+function Todo(props) {
+	return (
+		<li>{props.todo.text}</li>
+	);
+}
+
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+	constructor() {
+		super()
+		this.state = {
+			todos: [],
+			value: '',
+		};
+
+		this.handleChange = this.handleChange.bind(this);
+		this.handleSubmit = this.handleSubmit.bind(this);
+	}
+
+	handleChange(event) {
+		this.setState({value: event.target.value});
+		//console.log("change :" + this.state.todos);
+	}
+
+	addTodo() {
+		//console.log(this.state.value);
+		if (this.state.value !== "") {
+			var newTodo = {
+				text: this.state.value,
+				id: Date.now(),
+			}
+
+			this.setState({
+				todos: [...this.state.todos, newTodo],
+			})
+		}
+	}
+	
+	handleSubmit(event) {
+		this.addTodo();
+		//console.log(this.state.todos);
+		this.setState({ value: '', });
+		event.preventDefault();
+	}
+
+	render() {
+    	return (
+			<div className="App">
+				<div className="header">
+					<h1>React TODO App</h1>
+					<form onSubmit={this.handleSubmit}>
+						<label>
+							Name:
+							<input type="text" value={this.state.value} onChange={this.handleChange} />
+						</label>
+						<input type="submit" value="Submit" />
+					</form>
+					<div className="todo-list">
+						<ul>
+							{this.state.todos.map((todo) => <Todo todo={todo} />)}
+						</ul>
+					</div>
+				</div>
+			</div>
+    	);
+	}
 }
 
 export default App;
