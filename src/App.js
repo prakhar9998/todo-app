0b5d87138f51c283bc003dad14a81844e86900b8
Prakhar Varshney <prakhar.varshney33@gmail.com>
2018-07-20 08:27:46 +0530

    cancel function

--- src/App.js
+++ src/App.js
@@ -1,13 +1,24 @@
 import React, { Component } from 'react';
 import './App.css';
 
-function Todo(props) {
+function Todo (props) {
 	return (
 		<li>
 			<div className="todo">
 				<span className="todo-text">{props.todo.text}</span>
-				<button className="delete-btn" onClick={props.onDelete}>X</button>
+				<button onClick={props.onEdit}>Edit</button>
+				<button className="delete-btn" onClick={props.onDelete}>Delete</button>
 			</div>
+		</li>
+	);
+}
+
+function TodoEdit(props) {
+	return (
+		<li>
+			<input type="text" placeholder="enter new task"/>
+			<button onClick={props.updateTodo}>Save</button>
+			<button onClick={props.onCancel}>Cancel</button>
 		</li>
 	);
 }
@@ -22,6 +33,7 @@ class App extends Component {
 
 		this.handleChange = this.handleChange.bind(this);
 		this.handleSubmit = this.handleSubmit.bind(this);
+		this.input = React.createRef();
 	}
 
 	handleChange(event) {
@@ -35,6 +47,7 @@ class App extends Component {
 			var newTodo = {
 				text: this.state.value,
 				key: Date.now(),
+				edit: false,
 			}
 
 			this.setState({
@@ -48,6 +61,22 @@ class App extends Component {
 		//console.log(this.state.todos);
 		this.setState({ value: '', });
 		event.preventDefault();
+	}
+
+	toggleEdit(key) {
+		/*const todoIndex = this.state.todos.findIndex(todo => todo.key === key);
+		const updatedTodo = { ...this.state.todos[todoIndex], edit: !this.state.todos[todoIndex].edit};
+		this.setState({
+			todos: [...this.state.todos.slice(0, todoIndex), updatedTodo, ...this.state.todos.slice(todoIndex + 1),],
+		});*/
+		this.setState({
+			todos: this.state.todos.map(todo => {
+				if (todo.key === key) {
+					todo = {...todo, edit: !todo.edit};
+				}
+				return todo;
+			})
+		})
 	}
 
 	removeAll() {
@@ -69,15 +98,26 @@ class App extends Component {
 					<h1>React TODO App</h1>
 					<form onSubmit={this.handleSubmit}>
 						<label>
-							<input className="input-todo" type="text" placeholder="Enter Task" value={this.state.value} onChange={this.handleChange} />
+							<input className="input-todo" type="text" placeholder="Enter Task" 
+							value={this.state.value} onChange={this.handleChange} />
 						</label>
 						<input className="submit-todo" type="submit" value="Submit" />
 					</form>
 					<div className="todo-list">
 						<ul>
-							{this.state.todos.map((todo) => <Todo
-							onDelete = {() => this.removeTodo(todo.key)}
-							todo={todo} />)}
+							{this.state.todos.map((todo) => {
+								if (!todo.edit) {
+									return <Todo todo={todo} 
+									onDelete={() => this.removeTodo(todo.key)}
+									onEdit={() => this.toggleEdit(todo.key)} />
+								} else {
+									return (
+										<TodoEdit todo={todo} 
+										onCancel={() => this.toggleEdit(todo.key)}
+										onSave={() => this.updateTodo(todo.key)}/>
+									);
+								}
+							})}
 						</ul>
 						<button onClick={() => this.removeAll()}>Clear All</button>
 					</div>
