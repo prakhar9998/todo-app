a4bc401d1c96d31f146611d352b01ebe3f304091
Prakhar Varshney <prakhar.varshney33@gmail.com>
2018-08-05 10:16:19 +0530

    fix style

--- src/App.js
+++ src/App.js
@@ -103,7 +103,7 @@ class App extends Component {
 	render() {
     	return (
 			<div className="App">
-			<h1>React TODO App</h1>
+				<h1>React TODO App</h1>
 				<div className="todo-page">
 					<form className="add-form" onSubmit={this.handleSubmit}>
 						<input className="input-todo" type="text" name="newTodo" placeholder="Enter New Task" 
